385828e199af2432a5a21cac831037d33c03a2f1
aws69 <awsalomari11469@gmail.com>
2023-06-18 12:10:21 +0300

    regex

--- rejex.test.js
+++ rejex.test.js
@@ -7,17 +7,23 @@ characters (no numbers and symbols) and it should end with capital A else return
 
 function capitalA(s){
     // Add your logic.
-    return;
+    if (/[^a-zA-Z]/.test(s) && s.endsWith('A')) {
+        return true;
+    }else{
+        return false;
 }
-
+}
 
 /* Write a function that take a string and return true if the the sting is following the emails pattern
 which end with io (example@example.io) */
 
 function ioEmail(email){
     // Add your logic.
-    return;
+    let emailTest = /^[a-z]+@+[a-z]+.io$/;
+    return emailTest.test(email);
+
 }
+
 
 /* You have a text that contain image names with their extention you need to write a function to 
 find all images in that text and return their names and extention in an array 
@@ -29,7 +35,13 @@ required extention are jpg, jpeg and png.
 function imagesSearcher(text){
     let arr = [];
     // Add your logic.
-    return arr
+    let imageTest = /\b([\w-]+\.(?:jpg|jpeg|png))\b/gi;
+    arr = text.match(imageTest);
+    if (arr) {
+        return arr;
+    } else {
+        return [];
+    }
 }
 
 
